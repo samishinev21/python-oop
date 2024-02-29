0a11b4e6a383dae90c34f5524400e2b8f4a17226
Sammy Mishinev <sam.mishinev@gmail.com>
2024-02-29 19:42:51 +0200

    Add vet

---
+++ 04-classes_and_objects-excercise/01-vet.py
@@ -0,0 +1,44 @@
+class Vet:
+    animals = []
+    storage = 0
+    space = 5
+    def __init__(self, name):
+        self.name = name
+        self.animals = []
+
+    def register_animal(self, animal_name):
+        if len(self.animals) < Vet.space:
+            self.animals.append(animal_name)
+            Vet.animals.append(animal_name)
+            Vet.storage += 1
+            return f"{animal_name} registered in the clinic"
+        
+        else:
+            return "Not enough space"
+        
+    def unregister_animal(self, animal_name):
+        if animal_name in self.animals:
+            self.animals.remove(animal_name)
+            Vet.animals.remove(animal_name)
+            Vet.storage -= 1
+            return f"{animal_name} usregistered successfully"
+
+        else:
+            return f"{animal_name} not in the clinic"
+        
+    def info(self):
+        return f"{self.name} has {len(self.animals)} animals. {Vet.space - Vet.storage} space left in clinic"
+    
+peter = Vet("Peter")
+george = Vet("George")
+print(peter.register_animal("Tom"))
+print(george.register_animal("Cory"))
+print(peter.register_animal("Fishy"))
+print(peter.register_animal("Bobby"))
+print(george.register_animal("Kay"))
+print(george.unregister_animal("Cory"))
+print(peter.register_animal("Silky"))
+print(peter.unregister_animal("Molly"))
+print(peter.unregister_animal("Tom"))
+print(peter.info())
+print(george.info())
