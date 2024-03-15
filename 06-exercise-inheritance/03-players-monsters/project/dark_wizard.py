0ffb1f0c6dcdba6bf549c957a75a8507e077c0f2
Sammy Mishinev <sam.mishinev@gmail.com>
2024-03-15 21:16:20 +0200

    Add players and monsters

---
+++ 06-exercise-inheritance/03-players-monsters/project/dark_wizard.py
@@ -0,0 +1,5 @@
+from project.wizzard import Wizzard
+
+class DarkWizzard(Wizzard):
+    def __init__(self, username, level):
+        super().__init__(username, level)
---
+++ 06-exercise-inheritance/03-players-monsters/project/soul_master.py
@@ -0,0 +1,5 @@
+from project.dark_wizard import DarkWizzard
+
+class SoulMaster(DarkWizzard):
+    def __init__(self, username, level):
+        super().__init__(username, level)
