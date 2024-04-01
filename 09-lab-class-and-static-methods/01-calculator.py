e1e9d4680159e47db2ed387930b0115603ad8209
Sammy Mishinev <sam.mishinev@gmail.com>
2024-04-01 17:10:15 +0300

    Calculator

--- 09-lab-class-and-static-methods/01-calculator.py
+++ 09-lab-class-and-static-methods/01-calculator.py
@@ -25,8 +25,3 @@ class Calculator:
             result -= args[i]
 
         return result
-    
-print(Calculator.add(5, 10, 4))
-print(Calculator.multiply(1, 2, 3, 5))
-print(Calculator.divide(100, 2))
-print(Calculator.subtract(90, 20, -50, 43, 7))
