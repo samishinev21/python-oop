0b93bc28d3ac07c9596549bb93d91c2994394d4a
Sammy Mishinev <sam.mishinev@gmail.com>
2024-04-19 16:13:41 +0300

    Add shapes

---
+++ 11-lab-polymorphism-and-abstraction/shapes.py
@@ -0,0 +1,32 @@
+from math import pi
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def calculate_area(self):
+        pass
+
+    @abstractmethod
+    def calculate_perimeter(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.__radius = radius
+
+    def calculate_area(self):
+        return pi * self.__radius * self.__radius
+    
+    def calculate_perimeter(self):
+        return 2 * pi * self.__radius
+    
+class Rectangle(Shape):
+    def __init__(self, height, width):
+        self.__height = height
+        self.__width = width
+
+    def calculate_area(self):
+        return self.__height * self.__width
+    
+    def calculate_perimeter(self):
+        return self.__height * 2 + self.__width * 2
