a78df515fa7afd0b41531eba1b80c74f7a5b1f01
Sammy Mishinev <sam.mishinev@gmail.com>
2024-04-23 17:15:24 +0300

    Remove lines

--- 12-exercise-polymorphism-and-abstraction/02-groups.py
+++ 12-exercise-polymorphism-and-abstraction/02-groups.py
@@ -25,20 +25,3 @@ class Group:
     
     def __getitem__(self, i):
         return f"Person {i}: {str(self.people[i])}"
-        
-p0 = Person('Aliko', 'Dangote')
-p1 = Person('Bill', 'Gates')
-p2 = Person('Warren', 'Buffet')
-p3 = Person('Elon', 'Musk')
-p4 = p2 + p3
-
-first_group = Group('__VIP__', [p0, p1, p2])
-second_group = Group('Special', [p3, p4])
-third_group = first_group + second_group
-
-print(len(first_group))
-print(second_group)
-print(third_group[0])
-
-for person in third_group:
-    print(person)
