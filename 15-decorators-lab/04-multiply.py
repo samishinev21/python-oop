f6a4d5f25631153a82b8db6908bd5529047b48f0
Sammy Mishinev <sam.mishinev@gmail.com>
2024-06-26 09:39:25 +0300

    Add multiply

---
+++ 15-decorators-lab/04-multiply.py
@@ -0,0 +1,15 @@
+def multiply(times):
+    def decorator(function):
+        def wrapper(number):
+            result = function(number)
+            return result * times
+        
+        return wrapper
+        
+    return decorator
+
+@multiply(5)
+def add_ten(number):
+    return number + 10
+
+print(add_ten(6))
